778077336c88b51cf106fbf25383131b7a33179c
deepekaa <deepekaakandhavel@gmail.com>
2026-09-08 12:40:51 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -4,4 +4,46 @@ import urllib.request
 
 def get_vid(query):
   try:
-    endcoded = urllib
+    endcoded = urllib.parse.quote(quer)
+    url = (
+      "https://www.youtube.com/results"
+      "?search_query=" + encoded
+    )
+    request = urllib.request.Request(
+      url,
+      headers={
+        "User-Agent":"MOzilla/5,0"
+      }  
+    )
+    data = urlllib.request.urlopen(
+      request,
+      timeout=5
+    ).read().decode("utf-8", errors="ignore")
+    ids = re.findall(
+      r'"videoId":"([^"]+)"',
+      data
+    )
+    
+    return ids[0] if ids else None
+
+except Exception:
+    return None
+
+def create_youtube_url(command):
+  text = command.lower().strip()
+  patterns = [
+    r"play\s+song\s+(.+)",
+    r"play\s+song\s+(.+)",
+    r"play\s+(.+)",
+    r"youtube\s+(.+)"
+  ]  
+
+  query = command
+
+  for pattern in patterns:
+    match = re.search(
+      pattern,
+      text
+    )
+    
+    
